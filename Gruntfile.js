f838fb3cceb7fd754ff2ae382108508f6e4d1c85
Amandine Dupays <amandine.dupays@gmail.com>
2015-07-29 12:06:37 +0200

    add watch task working

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    
+
     concat: {
       dist: {
         src: ['src/react.js', 'src/device-type.js', 'src/estimate.js', ],
@@ -19,12 +19,22 @@ module.exports = function(grunt) {
           'js/www-mobiloc.js': 'src/app.js'
         }
       }
+    },
+    watch: {
+      scripts: {
+        files: ['js/*.js', 'src/*.js'],
+        tasks: ['concat', 'babel'],
+        options: {
+          spawn: false,
+        },
+      },
     }
   });
   
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-babel');
-  grunt.registerTask("default", ["concat", "babel"]);
+  grunt.registerTask("default", ["watch", "concat", "babel"]);
   
 };
